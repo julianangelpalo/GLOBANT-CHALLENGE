c2936bc6574300b807db96b98cf8a04ecbab02ae
Julián Ángel <82555731+julianangelpalo@users.noreply.github.com>
2023-09-28 14:49:50 -0500

    Update Challenge2_1.py

--- globant/Challenge2_1.py
+++ globant/Challenge2_1.py
@@ -3,55 +3,30 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Define a route for the metric
-@app.route('/metrics/employee_hiring', methods=['GET'])
-def employee_hiring_metrics():
+# Define una ruta para la métrica
+@app.route('/metricas/contratacion_empleados', methods=['GET'])
+def metricas_contratacion_empleados():
     try:
-        # Connect to the SQLite database
+        # Conéctate a la base de datos SQLite
         conn = sqlite3.connect('my_database_globant.db')
         cursor = conn.cursor()
 
-        # Execute your SQL query to calculate the metric
+        # Ejecuta tu consulta SQL para calcular la métrica
         query = """
-        SELECT
-            d.department,
-            j.job,
-            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '01' AND '03' THEN 1 ELSE 0 END) AS Q1,
-            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '04' AND '06' THEN 1 ELSE 0 END) AS Q2,
-            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '07' AND '09' THEN 1 ELSE 0 END) AS Q3,
-            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '10' AND '12' THEN 1 ELSE 0 END) AS Q4
-        FROM
-            departments d
-        INNER JOIN
-            hired_employees he ON d.id = he.department_id
-        INNER JOIN
-            jobs j ON he.job_id = j.id
-        WHERE
-            strftime('%Y', he.datetime) = '2021'
-        GROUP BY
-            d.department,
-            j.job
-        ORDER BY
-            d.department,
-            j.job;
+        -- Tu consulta SQL aquí --
         """
 
         cursor.execute(query)
 
-        # Fetch the results as a list of dictionaries
+        # Obtiene los resultados como una lista de diccionarios
         results = []
         for row in cursor.fetchall():
-            department, job, q1, q2, q3, q4 = row
+            # Procesa los resultados aquí
             results.append({
-                "Department": department,
-                "Job": job,
-                "Q1": q1,
-                "Q2": q2,
-                "Q3": q3,
-                "Q4": q4
+                # Define las claves y valores aquí
             })
 
-        return jsonify({"results": results})
+        return jsonify({"resultados": results})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
