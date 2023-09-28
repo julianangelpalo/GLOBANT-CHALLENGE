ac78c1f3761403af1a9b1457960bd6cc314ef1b0
Julián Ángel <82555731+julianangelpalo@users.noreply.github.com>
2023-09-28 14:51:42 -0500

    Update Challenge2_1.py

--- globant/Challenge2_1.py
+++ globant/Challenge2_1.py
@@ -3,30 +3,53 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Define una ruta para la métrica
-@app.route('/metricas/contratacion_empleados', methods=['GET'])
-def metricas_contratacion_empleados():
+@app.route('/metrics/employee_hiring', methods=['GET'])
+def employee_hiring_metrics():
     try:
-        # Conéctate a la base de datos SQLite
+
         conn = sqlite3.connect('my_database_globant.db')
         cursor = conn.cursor()
 
-        # Ejecuta tu consulta SQL para calcular la métrica
+
         query = """
-        -- Tu consulta SQL aquí --
+        SELECT
+            d.department,
+            j.job,
+            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '01' AND '03' THEN 1 ELSE 0 END) AS Q1,
+            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '04' AND '06' THEN 1 ELSE 0 END) AS Q2,
+            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '07' AND '09' THEN 1 ELSE 0 END) AS Q3,
+            SUM(CASE WHEN strftime('%m', he.datetime) BETWEEN '10' AND '12' THEN 1 ELSE 0 END) AS Q4
+        FROM
+            departments d
+        INNER JOIN
+            hired_employees he ON d.id = he.department_id
+        INNER JOIN
+            jobs j ON he.job_id = j.id
+        WHERE
+            strftime('%Y', he.datetime) = '2021'
+        GROUP BY
+            d.department,
+            j.job
+        ORDER BY
+            d.department,
+            j.job;
         """
 
         cursor.execute(query)
 
-        # Obtiene los resultados como una lista de diccionarios
         results = []
         for row in cursor.fetchall():
-            # Procesa los resultados aquí
+            department, job, q1, q2, q3, q4 = row
             results.append({
-                # Define las claves y valores aquí
+                "Department": department,
+                "Job": job,
+                "Q1": q1,
+                "Q2": q2,
+                "Q3": q3,
+                "Q4": q4
             })
 
-        return jsonify({"resultados": results})
+        return jsonify({"results": results})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
